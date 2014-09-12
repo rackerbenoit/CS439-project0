c1f3b893ae5c0955d9a86e9ad05c2eff9e0c4c12
paulbeno <paulbeno@cs.utexas.edu>
2014-09-11 22:12:27 -0500

    Got handle partly working

--- handle.c
+++ handle.c
@@ -17,14 +17,50 @@
  * Finally, loop forever, printing "Still here\n" once every
  * second.
  */
+
+
+//Paul is driving
+
+void handler(int sig)
+{
+	ssize_t bytes; 
+	const int STDOUT = 1; 
+	bytes = write(STDOUT, "Nice try.\n", 10); 
+	if(bytes != 10) 
+   	exit(-999);	
+}
+
+void alarmhandler(int sig)
+{
+
+	struct timespec req;
+	req.tv_sec = 1;
+	struct timespec rem;
+	req.tv_nsec = 0;
+
+	int interrupt = nanosleep(&req, &rem); 
+	printf("%s", "Still here\n");
+	if(interrupt  == -1)
+	{
+		nanosleep(&rem, &rem);
+	}	
+}
+
 int main(int argc, char **argv)
 {
 	/* print out this process's ID - Zoe is driving*/
 	pid_t pid;
 	pid = getpid();
-	printf(stdout, "This process's ID is %d\n", pid);
- 	
+	fprintf(stdout, "This process's ID is %d\n", pid);
 	
+
+	//Paul is driving
+	Signal(SIGINT, handler);
+	Signal(SIGALRM, alarmhandler);
+
+	while(1){
+		alarm(1);
+	}
 	return 0;
 }
 
